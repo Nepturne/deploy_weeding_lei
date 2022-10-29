7b9fa8b09f14a00f7b48d93fcabadb75a29681b6
Lucas Pereira Cabral <lucas.cabral@edge.ufal.br>
2022-10-29 15:57:08 -0300

    rkl

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 // Após informar o usuário e a senha do banco , será efetuada a conexão com o seu banco de dados MySQL.
-const connection = new Sequelize("casamentolei", "root", "root", {
+const connection = new Sequelize("casamentolei", "b07230eae350ef", "25234154", {
     logging: false, // Retirar Log de Queris no ORM Sequelize
-    host: "localhost",
+    host: "us-cdbr-east-06.cleardb.net",
     port: "3306",
     dialect: 'mysql',
     timezone: "-03:00",
